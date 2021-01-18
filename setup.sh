604dddf409a4bafd609b2238d004b097a0d10d49
bmicheal77 <bmicheal@student.21-school.ru>
2021-01-18 14:19:10 +0300

    lol

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ minikube start --driver=virtualbox
 eval $(minikube docker-env)
 docker build -t nginx_image srcs/nginx/
 minikube addons enable dashboard
-minikube dashboard&
+minikube addons enable metallb
+minikube dashboard &
+kubectl apply -f srcs/configmap.yaml
 kubectl apply -f srcs/nginx/nginx.yaml
